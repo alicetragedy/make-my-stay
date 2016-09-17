40e89eae0a6f1bd9a85c122a94a7f1292483fd92
alicetragedy <girl@alicetragedy.org>
2016-09-17 16:05:46 +0200

    add first part of bot with basic greeting

--- main.py
+++ main.py
@@ -33,35 +33,39 @@ def post_webhook():
 
         if "message" in messaging_event:
 
-          sender_id = messaging_event['sender']['id']
+          sender_id = messaging_event['sender']['id'] #sender id of person sending the message
+          recipient_id = messaging_event["recipient"]["id"] #recipient id (facebook page id)
 
           if 'text' in messaging_event['message']:
             message_text = messaging_event['message']['text']
-            image = "http://cdn.shopify.com/s/files/1/0080/8372/products/tattly_jen_mussari_hello_script_web_design_01_grande.jpg"
-            element = create_generic_template_element("Hello", image, message_text)
-            reply_with_generic_template(sender_id, [element])
-            #do_rules(sender_id, message_text)
+            #image = "http://cdn.shopify.com/s/files/1/0080/8372/products/tattly_jen_mussari_hello_script_web_design_01_grande.jpg"
+            #element = create_generic_template_element("Hello", image, message_text)
+            #reply_with_generic_template(sender_id, [element])
+            welcome(sender_id, message_text)
             #reply(sender_id, 'Hello there!')
 
   return "ok", 200
-
 
 def get_url(url):
     result = request.get(url)
     return json.loads(result.content)
 
 
-def do_rules(recipient_id, message_text):
+def welcome(recipient_id, message_text):
     rules = {
-        "Hello": "World",
-        "Foo": "Bar"
+        "hello": "hello",
+        "hi": "Hi there and welcome to the Hotel am Brillantengrund. I can check if rooms are available and give you advice for daily activities.",
+        "get started": "Hi there and welcome to the Hotel am Brillantengrund. I can check if rooms are available and give you advice for daily activities."
+        #"Foo": "Bar"
     }
 
     if message_text in rules:
-        reply_with_text(recipient_id, rules[message_text])
-
+      reply_with_buttons(recipient_id)
+        
     else:
-        reply_with_text(recipient_id, "You have to write something I understand ;)")
+        reply_with_text(recipient_id, "I'm sorry, I don't understand you. Click the button below to get started!")
+        #get_started(recipient_id, )
+    
 
 
 def reply_with_text(recipient_id, message_text):
@@ -69,6 +73,31 @@ def reply_with_text(recipient_id, message_text):
         "text": message_text
     }
     reply_to_facebook(recipient_id, message)
+
+def reply_with_buttons(recipient_id):
+  message = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "button",
+        "text": "Hi there and welcome to the Hotel am Brillantengrund. I can check if rooms are available and give you advice for daily activities.",
+        "buttons":[
+            {
+              "type": "postback",
+              "title": "Room Availability",
+              "payload": "ROOM_AVAILABILITY"
+            },
+            {
+              "type":"postback",
+              "title":"Daily Tip",
+              "payload":"DAILY_TIP"
+            }
+          ]
+      }
+    }
+  }
+  reply_to_facebook(recipient_id, message)
+  #received_postback(sender_id, message)
 
 
 def reply_with_generic_template(recipient_id, elements):
